c8bf6f36dea07bb48336ae2abaf686e20feffa82
Banti6541 <banti6541@gmail.com>
2022-09-20 12:37:10 +0530

    Implemented static Sub-Header

--- dukart/src/App.js
+++ dukart/src/App.js
@@ -1,10 +1,12 @@
 import Products from "./components/Products/Products";
-import Header from "./components/Header";
+import Header from "./components/Layouts/Header";
+import Subheader from "./components/Layouts/Subheader";
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <Subheader/>
       <Products/>
     </div>
   );
--- dukart/src/components/Layouts/Header.js
+++ dukart/src/components/Layouts/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <header>
             <div className="nav-brand">
                 <a to="/">
-                    <span>AmaKart</span>
+                    <span>Dukart</span>
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="30"
                         height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round"
                         strokeLinejoin="round">
