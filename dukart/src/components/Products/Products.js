bed1b3b24efded6afc773900b9476395f482c4f3
Banti6541 <banti6541@gmail.com>
2022-09-21 18:24:14 +0530

    Fetched list items from firebase using axios.

--- dukart/src/components/Products/Products.js
+++ dukart/src/components/Products/Products.js
@@ -1,51 +1,43 @@
 import ListItem from "./ListItems/ListItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Products = () => {
 
-    const [items, setItems] = useState([
-        {
-            id: 0,
-            dicountedPrice: 340,
-            price: 450,
-            title: "Title of the Item",
-            thumbnail: "placeholder.png"
-        },
-        {
-            id: 1,
-            dicountedPrice: 500,
-            price: 650,
-            title: "Title of the Item",
-            thumbnail: "placeholder.png"
-        },
-        {
-            id: 2,
-            dicountedPrice: 80,
-            price: 100,
-            title: "Title of the Item",
-            thumbnail: "placeholder.png"
-        },
-        {
-            id: 3,
-            dicountedPrice: 220,
-            price: 400,
-            title: "Title of the Item",
-            thumbnail: "placeholder.png"
-        }
-    ])
-    
+    const [items, setItems] = useState([])
 
-    return (
-        <div className="product-list">
-            <div className="product-list--wrapper">
-                {
-                    items.map(item => {
-                        return (<ListItem key={item.id} data = {item}/>)
-                    })
-                }
-            </div>
+    useEffect(() => {
+        async function fetchItems() {
+            try {
+                const response = await axios.get(`https://dukart-db-default-rtdb.firebaseio.com/items.json`)
+                const data = response.data
+                const transformedData = data.map((item, index) => {
+                    return {
+                        ...item,
+                        id: index
+                    }
+                })
+                setItems(transformedData)
+            }
+            catch (error) {
+                console.log(error)
+                alert("Error occured")
+            }
+        }
+        fetchItems();
+    }, [])
+
+return (
+    <div className="product-list">
+        <div className="product-list--wrapper">
+            {
+                items.map(item => {
+                    return (<ListItem key={item.id} data={item} />)
+                })
+            }
         </div>
-    )
+    </div>
+)
 }
 
 export default Products;
