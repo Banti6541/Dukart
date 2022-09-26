2de66fb8b55cd2cbf3de90946eafba81ae93497b
Banti6541 <banti6541@gmail.com>
2022-09-26 15:14:23 +0530

    Updating cart value is done.

--- dukart/src/components/Products/Products.js
+++ dukart/src/components/Products/Products.js
@@ -3,10 +3,11 @@ import Loader from "../UI/Loader";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Products = () => {
+const Products = ({ onAddItem, onRemoveItem }) => {
 
     const [items, setItems] = useState([])
     const [loader, setLoader] = useState(true)
+    const [presentItems, setPresentItems] = useState([])
 
     useEffect(() => {
         async function fetchItems() {
@@ -32,20 +33,40 @@ const Products = () => {
         fetchItems();
     }, [])
 
-return (
-    <>
-    <div className="product-list">
-        <div className="product-list--wrapper">
-            {
-                items.map(item => {
-                    return (<ListItem key={item.id} data={item} />)
-                })
-            }
-        </div>
-    </div>
-    { loader && <Loader/> }
-    </>
-)
+    const handleAddItem = id => {
+        if(presentItems.indexOf(id) > -1)
+        {
+            return;
+        }
+        setPresentItems([...presentItems, id])
+        onAddItem();
+    }
+
+    const handleRemoveItem = id => {
+        let index = presentItems.indexOf(id)
+        if(index > -1)
+        {
+            let items = [...presentItems]
+            items.splice(index, 1)
+            setPresentItems([...items])
+            onRemoveItem();
+        }
+    }
+
+    return (
+        <>
+            <div className="product-list">
+                <div className="product-list--wrapper">
+                    {
+                        items.map(item => {
+                            return (<ListItem onAdd={handleAddItem} onRemove={handleRemoveItem} key={item.id} data={item} />)
+                        })
+                    }
+                </div>
+            </div>
+            {loader && <Loader />}
+        </>
+    )
 }
 
 export default Products;
